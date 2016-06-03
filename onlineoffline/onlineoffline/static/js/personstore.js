32e131bb196c1d63497785a8e6639569e3fb3b5c
Joshua Brooks <josh.vdbroek@gmail.com>
2016-06-03 14:28:43 +0900

    contactlist example code cleanup

--- onlineoffline/onlineoffline/static/js/personstore.js
+++ onlineoffline/onlineoffline/static/js/personstore.js
@@ -1,31 +1,28 @@
-
-
 function PersonStore(persons) {
   riot.observable(this); // Riot provides our event emitter.
   var self = this;
   self.persons = persons; // From Django context provided in the template
-  self.persons = []; // Let's start fresh
-  self.edit={};
+  self.edit={}; // Store the Person currently being edited
 
     // Request the latest person list from DRF
   self.on('reload_from_server', function(){
-      xhr = $.get('/contactlist/api/person/?format=json')
+      xhr = $.get('/contactlist/api/person/?format=json');
       xhr.success(function(result) {
           self.persons = result.results;
           self.trigger('persons_changed', self.persons)
       })
   });
 
-  // Sae the current state of persons to 'Lawnchair' for offline storage
+  // Save the current state of persons to 'Lawnchair' for offline storage
   self.on('save_to_local', function() {
       Lawnchair({name: 'persons'}, function (store) {
           store.nuke(); // Clear all present items
           for (var indexof in self.persons) {
-              p = persons[indexof]
+              var p = self.persons[indexof];
               store.save({key: p.id, data: p})
           }
       });
-    })
+    });
 
   // Load the current state of persons saved in Lawnchair
   self.on('from_local', function(){
@@ -50,7 +47,6 @@ function PersonStore(persons) {
   });
 
   self.on('person_remove', function(e) {
-      console.log(e)
       var form = $(e.currentTarget);
       var xhr = $.ajax({
           url: form.attr('action'),
@@ -64,12 +60,11 @@ function PersonStore(persons) {
           }
         });
 
-    console.log(xhr);
     xhr.success(function(result) {
         self.persons.splice(self.persons.indexOf(e.item),1);
         self.trigger('persons_changed', self.persons);
         self.edit = {};
-    })
+    });
     self.trigger('persons_changed', self.persons)
   });
 
@@ -95,7 +90,7 @@ function PersonStore(persons) {
         });
 
     xhr.success(function(result) {
-        self.persons[self.persons.indexOf(item)] = result
+        self.persons[self.persons.indexOf(item)] = result;
         self.trigger('persons_changed', self.persons);
         self.trigger('person_edit_hide');
 
