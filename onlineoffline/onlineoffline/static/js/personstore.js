9048b8fab45d3c3394a5305279c4581f623eedf8
Joshua Brooks <josh.vdbroek@gmail.com>
2016-06-05 18:28:40 +0900

    Fixes #2 - RiotJS clientside request caching

--- onlineoffline/onlineoffline/static/js/personstore.js
+++ onlineoffline/onlineoffline/static/js/personstore.js
@@ -1,100 +1,101 @@
 function PersonStore(persons) {
-  riot.observable(this); // Riot provides our event emitter.
-  var self = this;
-  self.persons = persons; // From Django context provided in the template
-  self.edit={}; // Store the Person currently being edited
+
+    riot.observable(this); // Riot provides our event emitter.
+
+    var modelName = 'person'; // Used to generate URLs for our API
+    var appName = 'contactlist';
+
+    var self = this;
+    self.persons = persons; // From Django context provided in the template
+    self.edit={}; // Store the Person currently being edited
 
     // Request the latest person list from DRF
-  self.on('reload_from_server', function(){
-      xhr = $.get('/contactlist/api/person/?format=json');
-      xhr.success(function(result) {
-          self.persons = result.results;
-          self.trigger('persons_changed', self.persons)
-      })
-  });
-
-  // Save the current state of persons to 'Lawnchair' for offline storage
-  self.on('save_to_local', function() {
-      Lawnchair({name: 'persons'}, function (store) {
-          store.nuke(); // Clear all present items
-          for (var indexof in self.persons) {
-              var p = self.persons[indexof];
-              store.save({key: p.id, data: p})
-          }
-      });
+    self.on('reload_from_server', function(){
+        var request = {
+            'url': '/contactlist/api/person/?format=json',
+            'method': 'GET',
+            'data': '',
+            'status': 'waiting',
+            'done': function (data, textStatus, jqXHR) {
+                self.persons = data.results;
+                console.log(self.persons);
+                self.trigger('persons_changed', self.persons)
+            }
+        };
+        RiotControl.trigger('request_add', request);
+        //RiotControl.trigger('request_do', request);
     });
 
-  // Load the current state of persons saved in Lawnchair
-  self.on('from_local', function(){
-      self.persons = []; // Clear present items in the page
-      Lawnchair({name:'persons'}).all(function(store){
-          for (var indexof in store){
-              self.persons.push(store[indexof].data)
-          }
-          self.trigger('persons_changed', self.persons)
-      })
-  });
-
-  self.on('person_add', function(newPerson) {
-      $.post('/contactlist/api/person/', newPerson, function(result) {
-        self.persons.push(result);
-        self.trigger('persons_changed', self.persons)
-    })
-  });
-
-  self.on('person_init', function() {
-    self.trigger('persons_changed', self.persons)
-  });
-
-  self.on('person_remove', function(e) {
-      var form = $(e.currentTarget);
-      var xhr = $.ajax({
-          url: form.attr('action'),
-          method: form.attr('method'),
-          data: '',
-          contentType: 'application/json',
-          processData: false,
-          headers: {
-              'Accept': 'application/json; q=1.0, */*',
-              'X-CSRFToken':form.find('[name=csrfmiddlewaretoken]').val()
-          }
+    // Save the current state of persons to 'Lawnchair' for offline storage
+    self.on('save_to_local', function() {
+        Lawnchair({name: 'persons'}, function (store) {
+            store.nuke(); // Clear all present items
+            for (var indexof in self.persons) {
+                var p = self.persons[indexof];
+                store.save({key: p.id, data: p})
+            }
         });
-
-    xhr.success(function(result) {
-        self.persons.splice(self.persons.indexOf(e.item),1);
-        self.trigger('persons_changed', self.persons);
-        self.edit = {};
     });
-    self.trigger('persons_changed', self.persons)
-  });
 
-  self.on('person_edit', function(e, item) {
+    // Load the current state of persons saved in Lawnchair
+    self.on('from_local', function(){
+        self.persons = []; // Clear present items in the page
+        Lawnchair({name:'persons'}).all(function(store){
+            for (var indexof in store){
+                self.persons.push(store[indexof].data)
+            }
+            self.trigger('persons_changed', self.persons)
+        })
+    });
 
-    var form = $(e.currentTarget);
-    var obj = {};
-    $.map(form.find('input, textarea').not('[name=csrfmiddlewaretoken],[type=submit]'), function(n, i)
+    self.on('person_init', function() {
+        self.trigger('persons_changed', self.persons)
+    });
+
+    self.on('person_add', function(newPerson) {
+
+        var done = function(data, textStatus, jqXHR){
+            console.log(data)
+            self.persons.push(data);
+            self.trigger('persons_changed', self.persons)
+        };
+
+        RiotControl.trigger('model_add', newPerson, 'contactlist', 'person', done);
+        // TODO: Indicate that this person is not actually on the server until we call request.do() on the request created
+        // self.persons.push(newPerson);
+    });
+
+    self.on('person_remove', function(e) {
+
+        var modelPk = e.item.id;
+        var done =  function (xhr) {
+            self.persons.splice(self.persons.indexOf(e.item), 1);
+            self.trigger('persons_changed', self.persons);
+            self.edit = {};
+        };
+
+        RiotControl.trigger('model_remove', appName, modelName, modelPk, done)
+    });
+
+    self.on('person_edit', function(e, item) {
+
+        var form = $(e.currentTarget);
+        var obj = {};
+        // Derive a JSON payload fpr our request from the form
+        $.map(form.find('input, textarea').not('[name=csrfmiddlewaretoken],[type=submit]'), function(n, i)
         {
             obj[n.name] = $(n).val();
         });
+        var newPerson = JSON.stringify(obj);
+        var done = function (data, textStatus, jqXHR) {
+            // Replace "person" record with the returned data
+            self.persons[self.persons.indexOf(item)] = data.result;
+            self.trigger('persons_changed', self.persons);
+            self.trigger('person_edit_hide')
+        };
 
-    var xhr = $.ajax({
-          url: form.attr('action'),
-          method: form.attr('method') || 'POST',
-          data: JSON.stringify(obj),
-          contentType: 'application/json',
-          processData: false,
-          headers: {
-              'Accept': 'application/json; q=1.0, */*',
-              'X-CSRFToken':form.find('[name=csrfmiddlewaretoken]').val()
-          }
-        });
-
-    xhr.success(function(result) {
-        self.persons[self.persons.indexOf(item)] = result;
-        self.trigger('persons_changed', self.persons);
-        self.trigger('person_edit_hide');
+        RiotControl.trigger('model_update', newPerson, 'contactlist', 'person', item.id, done);
 
     })
-  });
 }
 
--- onlineoffline/onlineoffline/static/js/requeststore.js
+++ onlineoffline/onlineoffline/static/js/requeststore.js
@@ -1,17 +1,126 @@
-function RequestStore(requests) {
+/*
+RequestStore
+Provides a riotjs store to hold/inspect request information before it is passed to the server, store results afterwards
+*/
+
+
+function RequestStore() {
+
     // Holds details of an AJAX request: provides a local history of changes which we submit to the server
     riot.observable(this);
     var self = this;
-    self.requests = requests; // Make this load from the server or from local storage
+    self.requests = []; // Make this load from the server or from local storage
 
-    self.on('request_add', function (url, method, data) {
-        self.requests.push({'url':url, 'method':method, 'data':data, 'status':'waiting'});
+    // Save the current state of persons to 'Lawnchair' for offline storage
+    self.on('shelve_requests', function() {
+        console.log('shelve_requests')
+        Lawnchair({name: 'requests'}, function (store) {
+            store.nuke(); // Clear all present items
+            for (var indexof in self.requests) {
+                var p = self.requests[indexof];
+                store.save({key: p.id, data: p})
+            }
+        });
+    });
+
+    // Load the current state of persons saved in Lawnchair
+    self.on('unshelve_requests', function(){
+        console.log('unshelve_requests')
+        self.requests = []; // Clear present items in the page
+        Lawnchair({name:'requests'}).all(function(store){
+            for (var indexof in store){
+                self.requests.push(store[indexof].data)
+            }
+            self.trigger('requests_changed', self.requests)
+        })
+    });
+
+    self.on('requests_init', function() {
         self.trigger('requests_changed', self.requests)
     });
 
-    self.on('request_update', function (e, item, xhr) {
-        self.requests.status = xhr.status;
+    self.on('request_add', function (request) {
+        self.requests.push({'url':request.url, 'method':request.method, 'data':request.data, 'status':'waiting', 'done':request.done});
+        console.log(self.requests);
+        RiotControl.trigger('requests_changed', self.requests);
+    });
+
+    self.on('request_update', function (item, xhr) {
+        console.log(item);
+        console.log(xhr);
+        item.status = xhr.status;
         self.trigger('requests_changed', self.requests)
     });
 
+    self.on('request_do', function(request) {
+
+        headers = {
+            'Accept': 'application/json; q=1.0, */*'
+        };
+        if (request.csrftoken !== undefined){
+            headers['X-CSRFToken'] = request.csrftoken
+        }
+
+        var xhr = $.ajax({
+            url: request.url,
+            method: request.method,
+            data: request.data,
+            contentType: 'application/json',
+            processData: false,
+            headers: {
+
+                'X-CSRFToken': request.csrftoken
+            }
+        }).done(function(data, textStatus, jqXHR) {
+            console.log('request.do: Success')
+            // Call any "success" function passed along with our xhr request
+            if ($.isFunction(request.done)){
+                console.log('request.do: call done()')
+                console.log(request.done)
+                request.done(data, textStatus, jqXHR);
+            }
+        }).always(function (data, textStatus, jqXHR) {
+            console.log(data)
+            console.log(textStatus)
+            console.log(jqXHR)
+            RiotControl.trigger('request_update', request, jqXHR);
+            RiotControl.trigger('requests_changed',self.requests);
+        }).fail(function(jqXHR, textStatus, errorThrown ){
+            console.log('Fail')
+            alert('Fail')
+        })
+    });
+
+    self.on('model_add', function(modelData, appName, modelName, done) {
+        var url = '/' + appName + '/api/' + modelName + '/';
+        RiotControl.trigger('request_add', {
+                'url': url,
+                'method': 'POST',
+                'data': modelData,
+                'done': done
+            }
+        );
+    });
+
+    self.on('model_remove', function(appName, modelName , modelPk, done){
+        var url = '/'+ appName+'/api/'+modelName+'/'+modelPk+'/';
+        RiotControl.trigger('request_add',{
+            'url': url,
+            'method': 'DELETE',
+            'data': '',
+            'done':done
+        })
+    });
+
+
+    self.on('model_update', function(modelData, appName, modelName, modelPk, done){
+        var url = '/' + appName + '/api/' + modelName + '/'+modelPk+'/';
+        RiotControl.trigger('request_add', {
+                'url': url,
+                'method': 'PUT',
+                'data': modelData,
+                'done': done
+            }
+        );
+    });
 }
